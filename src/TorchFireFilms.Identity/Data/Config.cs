739829829b65f19525fb1964933cf1a6009bbadf
jrovny <jrovny@JROVNYDELL7540>
2021-01-09 22:17:13 -0600

    Add IS4 config class

---
+++ src/TorchFireFilms.Identity/Data/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace TorchFireFilms.Identity.Data
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                    new IdentityResource[]
+                    {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                    };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientName = "TorchFireFilms",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RedirectUris = { "https://localhost:5010/signin-redirect" },
+                    PostLogoutRedirectUris = { "https://localhost:5010" },
+                    AllowedCorsOrigins = { "https://localhost:5010" },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "scope1"
+                    }
+                }
+            };
+    }
+}

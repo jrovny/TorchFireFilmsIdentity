145a4190f16aa165efffd7f13f977a6a94082942
jrovny <jrovny@JROVNYDELL7540>
2021-01-09 22:16:21 -0600

    Remove Sqlite reference in startup class

--- src/TorchFireFilms.Identity/Startup.cs
+++ src/TorchFireFilms.Identity/Startup.cs
@@ -27,9 +27,6 @@ namespace TorchFireFilms.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();

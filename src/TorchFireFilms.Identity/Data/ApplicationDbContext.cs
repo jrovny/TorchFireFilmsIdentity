080c6642faeec3acd3d50d750ae8238d7a4952e6
jrovny <jrovny@JROVNYDELL7540>
2021-01-09 22:34:03 -0600

    Add snake case scaffolding code in app db context

--- src/TorchFireFilms.Identity/Data/ApplicationDbContext.cs
+++ src/TorchFireFilms.Identity/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.NameTranslation;
 
 namespace TorchFireFilms.Identity.Data
 {
@@ -11,6 +9,59 @@ namespace TorchFireFilms.Identity.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(GetNewTableName(entity.GetTableName()));
+
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(property.GetColumnName()));
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.SetName(NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(key.GetName()));
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.SetConstraintName(NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(key.GetConstraintName()));
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.SetDatabaseName(NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(index.GetDatabaseName()));
+                }
+            }
+
+            builder.Entity<ApplicationUser>().Property(u => u.CreatedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationUser>().Property(u => u.ModifiedDate).HasDefaultValueSql("now()");
+
+            builder.Entity<ApplicationRole>().Property(u => u.CreatedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationRole>().Property(u => u.ModifiedDate).HasDefaultValueSql("now()");
+
+            builder.Entity<ApplicationUserClaim>().Property(u => u.CreatedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationUserClaim>().Property(u => u.ModifiedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationUserClaim>().Property(u => u.Deleted).HasDefaultValueSql("false");
+
+            builder.Entity<ApplicationUserRole>().Property(u => u.CreatedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationUserRole>().Property(u => u.ModifiedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationUserRole>().Property(u => u.Deleted).HasDefaultValueSql("false");
+
+            builder.Entity<ApplicationRoleClaim>().Property(u => u.CreatedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationRoleClaim>().Property(u => u.ModifiedDate).HasDefaultValueSql("now()");
+            builder.Entity<ApplicationRoleClaim>().Property(u => u.Deleted).HasDefaultValueSql("false");
+        }
+
+        private string GetNewTableName(string name)
+        {
+            return NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(
+                name.TrimEnd('s').Replace("AspNet", "Identity"));
         }
     }
 }

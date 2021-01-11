deb38196598495f6ff920bb428ba9df50290fda3
jrovny <jrovny@JROVNYDELL7540>
2021-01-10 22:41:53 -0600

    Add default identity scaffolding

---
+++ src/TorchFireFilms.Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TorchFireFilms.Identity.Data;
+
+[assembly: HostingStartup(typeof(TorchFireFilms.Identity.Areas.Identity.IdentityHostingStartup))]
+namespace TorchFireFilms.Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

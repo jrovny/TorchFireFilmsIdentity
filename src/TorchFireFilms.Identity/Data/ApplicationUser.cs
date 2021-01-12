a0c793baffa5c7b3105e176fb43dcbfe4bdab547
jrovny <jrovny@JROVNYDELL7540>
2021-01-11 21:47:41 -0600

    Add first and last name to application user class

--- src/TorchFireFilms.Identity/Data/ApplicationUser.cs
+++ src/TorchFireFilms.Identity/Data/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace TorchFireFilms.Identity.Data
 {
     public class ApplicationUser : IdentityUser<int>
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
     }

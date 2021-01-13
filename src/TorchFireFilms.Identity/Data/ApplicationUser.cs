cdd143fb32537f81a642a50a2abcc60489d0ecd3
jrovny <jrovny@JROVNYDELL7540>
2021-01-12 20:24:43 -0600

    Add PersonalData attribute to first and last name properties

--- src/TorchFireFilms.Identity/Data/ApplicationUser.cs
+++ src/TorchFireFilms.Identity/Data/ApplicationUser.cs
@@ -5,7 +5,9 @@ namespace TorchFireFilms.Identity.Data
 {
     public class ApplicationUser : IdentityUser<int>
     {
+        [PersonalData]
         public string FirstName { get; set; }
+        [PersonalData]
         public string LastName { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }

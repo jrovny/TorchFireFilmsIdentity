55be1e7f7494f3f9791bc701b177a7f31263ce5a
jrovny <jrovny@JROVNYDELL7540>
2021-01-23 15:42:23 -0600

    Add serilog

--- src/TorchFireFilms.Identity/Program.cs
+++ src/TorchFireFilms.Identity/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace TorchFireFilms.Identity
 {
@@ -18,6 +21,19 @@ namespace TorchFireFilms.Identity
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, logger) =>
+                {
+                    var isDevelopment = context.HostingEnvironment.IsDevelopment();
+                    logger.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+                    logger.Enrich.FromLogContext();
+                    if (isDevelopment)
+                        logger.WriteTo.Console();
+                    logger.WriteTo.File(new JsonFormatter(),
+                        isDevelopment ? @".\log\log-.txt" : @"./log/log-.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: null,
+                        rollOnFileSizeLimit: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
